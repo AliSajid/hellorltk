eeeb9d707a074bdafc0bf6c195fc8fc83686415a
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-08-23 11:17:20 -0400

    feat: added the player display on the screen
    Signed-off-by: Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
-use rltk::{GameState, Rltk};
+use rltk::{GameState, Rltk, VirtualKeyCode, RGB};
+use specs::prelude::*;
+use specs_derive::Component;
+use std::cmp::{max, min};
 
-struct State {}
+struct State {
+    ecs: World,
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "Hello World!")
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
+}
+
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
 }
 
 fn main() -> rltk::BError {
@@ -14,6 +37,24 @@ fn main() -> rltk::BError {
     let context = RltkBuilder::simple80x50()
         .with_title("My Test Roguelike")
         .build()?;
-    let gs = State {};
+    let mut gs = State { ecs: World::new() };
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    // Create Player Entity
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+
     rltk::main_loop(context, gs)
+}
+
+fn create_player(mut gs: &State) {
+    todo!("Will Extract Player Creation to its own function")
 }
